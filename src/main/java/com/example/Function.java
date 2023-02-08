549badd9ce3686d315469b5f172c0d14a372b4a8
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-02-08 13:04:14 -0800

    Initial commit

---
+++ src/main/java/com/example/Function.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import java.util.Optional;
+
+public class Function {
+
+    private static final TelemetryClient telemetryClient = new TelemetryClient();
+
+    @FunctionName("HttpExample")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                    name = "req",
+                    methods = {HttpMethod.GET, HttpMethod.POST},
+                    authLevel = AuthorizationLevel.ANONYMOUS)
+            HttpRequestMessage<Optional<String>> request) {
+
+        telemetryClient.trackMetric("test", 123);
+
+        return request.createResponseBuilder(HttpStatus.OK).body("Hello world!").build();
+    }
+}
